ae5a29aebee04b4291e96a7c8d5891140c017803
pos002 <oparsheva12@gmail.com>
2025-09-26 19:43:40 +0300

    initial commit

--- lab_2/lab_2.cpp
+++ lab_2/lab_2.cpp
@@ -10,7 +10,7 @@ public:
     Vector()
     {
         this->data = nullptr;
-        this->data = 0;
+        this->size = 0;
         this->flag_memory = 1;
     }
 
@@ -89,7 +89,6 @@ public:
         if (data != nullptr && flag_memory)
         {
             delete[] this->data;
-
         }
     }
 
@@ -117,17 +116,19 @@ public:
         if (size <= index)
         {
             cout << "Index out of range" << endl;
+            return 0;
         }
         return data[index];
     }
 
     double Dot(const Vector& other)
     {
-        double dot = 0.0;
         if (this->size != other.size)
         {
-            cout << "Sizes are difference!" << endl;
+            cout << "Sizes are different!" << endl;
+            return 0;
         }
+        double dot = 0.0;
         for (size_t i = 0; i < this->size; i++)
         {
             dot += this->data[i] * other.data[i];
@@ -169,17 +170,24 @@ public:
     }
     void PrintVector()
     {
-        cout << "[";
-        for (size_t i = 0; i < size; i++)
+        if (size == 0)
         {
-            cout << data[i];
-            if (i < size - 1)
-            {
-                cout << ", ";
-            }
+            cout << "[]" << endl;
         }
-        cout << "] (flag_memory: " << flag_memory << ")" << endl;
-    }
+        else
+        {
+            cout << "[";
+            for (size_t i = 0; i < size; i++)
+            {
+                cout << data[i];
+                if (i < size - 1)
+                {
+                    cout << ", ";
+                }
+            }
+            cout << "] (flag_memory: " << flag_memory << ")" << endl;
+        }
+        }
 
 private:
     double* data;
@@ -191,5 +199,105 @@ private:
 
 int main()
 {
-    Vector vector;
+    setlocale(LC_ALL, "ru");
+    cout << "Демонстрация работы класса Vector и его конструкторов" << endl;
+
+    cout << "\n---Конструктор по умолчанию, создающий пустой вектор---" << endl;
+    Vector empty_v;
+    empty_v.PrintVector();
+
+    cout << "\n---Конструктор с параметром размера, создающий вектор заданного размера, инициализированный указанным значением---" << endl;
+    Vector v_size(10);
+    v_size.PrintVector();
+
+    cout << "\n---Конструктор принимающий размер и значение, создающий вектор заданного размера, инициализирвоанный указанным значением---" << endl;
+    Vector v_1(10, 5.1);
+    v_1.PrintVector();
+
+    cout << "\n---Конструктор, принимающий указатель на внешний массив и его размер---" << endl;
+    double external_data[] = {1.1, 3.4, 5.6};
+    Vector v_2(external_data, 3);
+    cout << "\nvector изначальный: ";
+    v_2.PrintVector();
+    external_data[1] = 10;
+    cout << "\nvector после изменения во внешнего массива: ";
+    v_2.PrintVector();
+
+    cout << "\n---Конструктор копирования, сохраняющий режим владения---" << endl;
+    Vector v_3_owning = v_1;
+    Vector v_3_non_owning = v_2;
+
+    cout << "\nкопия владеющего вектора: ";
+    v_3_owning.PrintVector();
+    cout << "\nкопия невладеющего вектора: ";
+    v_3_non_owning.PrintVector();
+
+    cout << "\n---Скалярное произведение векторов---" << endl;
+    Vector a(3, 3);
+    Vector b(3, 2);
+    double dot_res = a.Dot(b);
+    cout << "a: ";
+    a.PrintVector();
+    cout << "b: ";
+    b.PrintVector();
+    cout << "результат скалярного перемножения: " << dot_res << endl;
+
+    cout << "\n---Скалярное произведение векторов разных размеров---" << endl;
+    Vector a_1(2, 3);
+    Vector b_1(3, 2);
+    double dot_res_1 = a_1.Dot(b_1);
+    cout << "a_1: ";
+    a_1.PrintVector();
+    cout << "b_1: ";
+    b_1.PrintVector();
+    cout << "результат скалярного перемножения: " << dot_res_1 << endl;
+
+    cout << "\n---Работа с элементами---" << endl;
+    Vector v_4(4);
+    v_4.SetElement(0, 10);
+    v_4.SetElement(1, 20);
+    v_4.SetElement(2, 30);
+    v_4.SetElement(3, 19);
+    cout << "v_4[2]: " << v_4.GetElement(2) << endl;
+
+    cout << "\n---Выход за границы массива---" << endl;
+    Vector v(3);
+    v.SetElement(0, 1);
+    v.SetElement(1, 2);
+    v.SetElement(2, 3);
+    cout << "v[3]: " << v.GetElement(3) << endl;
+
+    // cout << "\n---2. Выход за границы массива---" << endl;
+    // Vector u(3);
+    // u.SetElement(0, 1);
+    // u.SetElement(1, 2);
+    // u.SetElement(2, 3);
+    // u.SetElement(3, 4);
+    // cout << "v[1]: " << u.GetElement(1) << endl;
+
+    cout << "\n---Метод Link---" << endl;
+    Vector v_5(2, 5.0);
+    cout << "v_5 до Link: ";
+    v_5.PrintVector();
+
+    double new_external[] = {100.0, 200.0};
+    v_5.Link(new_external, 2);
+    cout << "v_5 после Link: ";
+    v_5.PrintVector();
+    
+    cout << "\n---Метод Unlink---" << endl;
+    double external[] = {7.7, 8.8};
+    Vector v_6(external, 2);
+    cout << "v_6 до Unlink: ";
+    v_6.PrintVector();
+    
+    v_6.Unlink();
+    cout << "v_6 после Unlink: ";
+    v_6.PrintVector();
+    
+    external[0] = 99.9;
+    cout << "v_6 после изменения external: ";
+    v_6.PrintVector();
+
+    return 0;
 }
